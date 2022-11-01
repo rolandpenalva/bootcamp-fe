7dbc8cced110cf29244e38a79109b1671e13aca4
Rolando <Roland@Roland-MacBook-Pro.local>
2022-10-31 21:07:43 -0600

    feat: login 1

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,7 +2,13 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "../../store/user";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-// import "./Login.css";
+import * as Yup from "yup";
+import "./Login.css";
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string().min(5, "Too Short!").required("Required"),
+  password: Yup.string().min(5, "Too Short!").required("Required"),
+});
 
 const Login = (prop) => {
   const dispatch = useDispatch();
@@ -11,26 +17,33 @@ const Login = (prop) => {
     <div>
       <h1>Login</h1>
       <Formik
-        initialValues={{ username: "", password: "" }}
-        // validationSchema={}
+        initialValues={{
+          username: "",
+          password: "",
+        }}
+        validationSchema={LoginSchema}
         onSubmit={(values) => {
+          console.log("Data::::");
           console.log(values);
-          dispatch(login(values));
         }}
       >
-        <Form onSubmit={(data) => console.log(data)}>
-          <label>User Name: </label>
-          <Field type="text" name="username" />
-          <ErrorMessage name="username" />
-          <br />
-          <label>password: </label>
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" />
-          <br />
-          <button type="submit" className="submitButton">
-            Submit
-          </button>
-        </Form>
+        {({ errors, touched }) => (
+          <Form>
+            <label>User Name: </label>
+            <Field type="text" name="username" id="username" />
+            {errors.username && touched.username ? (
+              <div className="error">{errors.username}</div>
+            ) : null}
+            <br />
+            <label>password: </label>
+            <Field type="password" name="password" id="password" />
+            {errors.password && touched.password ? (
+              <div className="error">{errors.password}</div>
+            ) : null}
+            <br />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
